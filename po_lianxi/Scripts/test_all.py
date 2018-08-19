ad893252a20a7af821d46db4ee8ceec47de855fd
CZP2018 <caibanxian10086@163.com>
2018-08-19 10:50:59 +0800

    加到暂存区2

--- po_lianxi/Scripts/test_all.py
+++ po_lianxi/Scripts/test_all.py
@@ -6,7 +6,7 @@ import allure, pytest
 # 2. allure serve report
 
 class Test_allure:
-    @pytest.mark.parametrize("a", [1, 2, 3])
+    @pytest.mark.parametrize("a", [1, 2, 3, 4])
     @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step('我是测试步骤001')
     def test_al(self, a):
